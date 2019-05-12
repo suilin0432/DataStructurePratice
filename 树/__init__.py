6cbf41bfd52c9a8e68817268ad2c834eca8cab27
隋霖 <401569798@qq.com>
2019-05-12 22:36:48 +0800

    add a readme file of the tree structure

---
+++ 树/__init__.py
@@ -0,0 +1,5 @@
+from .BinarySearchTree import BinarySearchTree
+from .BinaryTree import BinaryTree
+from .PriorityQueue import PriorityQueue
+
+__all__ = ["BinarySearchTree", "BinaryTree", "PriorityQueue"]
