a5b9622c003773a9a14404426c4b4fcef25c18bb
隋霖 <401569798@qq.com>
2019-05-13 14:12:55 +0800

    finish the basic function of the basicGraph

--- 图/BasicGraph.py
+++ 图/BasicGraph.py
@@ -9,16 +9,37 @@ class BasicGraph(object):
         self.mark = [0] * nodeNumber
 
     def firstEdge(self, node):
-        assert node <
+        assert node < self.nodeNumber
         for i in range(self.nodeNumber):
             if self.matrix[node][i] != self.MAXINT:
                 return (node, i)
         return None
 
     def nextEdge(self, node, dst):
+        assert node < self.nodeNumber and dst < self.nodeNumber
         for i in range(dst+1, self.nodeNumber):
             if self.matrix[node][i] != self.MAXINT:
                 return (node, i)
         return None
 
     def isEdge(self, node, dst):
+        assert node < self.nodeNumber and dst < self.nodeNumber
+        return self.matrix[node][dst] != self.matrix
+
+    # 假设每两个节点之间只能有一条边
+    def setEdge(self, src, dst, wt):
+        assert wt < self.MAXINT
+        assert src < self.nodeNumber and dst < self.nodeNumber
+        if self.matrix[src][dst] != self.MAXINT:
+            self.edgeNumber += 1
+        self.matrix[src][dst] = wt
+
+    def delEdge(self, src, dst):
+        assert src < self.nodeNumber and dst < self.nodeNumber
+        if self.matrix[src][dst] != self.MAXINT:
+            self.matrix[src][dst] = self.MAXINT
+            self.edgeNumber -= 1
+
+    def weight(self, src, dst):
+        assert src < self.nodeNumber and dst < self.nodeNumber
+        return self.matrix[src][dst]
